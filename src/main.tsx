4c21774770b66e3cf58e135c594fccd51927cd81
David Neuman <david.m.neuman@gmail.com>
2023-11-23 15:50:22 -0500

    Add react-query and Mantine providers

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { MantineProvider } from '@mantine/core'
+import '@mantine/core/styles.css'
+import '@mantine/dates/styles.css'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider
+      theme={{
+        fontFamily: 'Inter, sans-serif',
+        headings: {
+          fontFamily: 'Inter, sans-serif',
+        },
+      }}
+    >
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </MantineProvider>
   </React.StrictMode>
 )

dd3751241d36b934f04203d5421f2fb094e97445
David Neuman <david.m.neuman@gmail.com>
2023-11-22 20:03:52 -0500

    Update api to allow for CORS Add imageKey to memories table Add ability to pass sort as query param

--- api.js
+++ api.js
@@ -1,11 +1,13 @@
-const express = require('express')
-const sqlite3 = require('sqlite3')
+import express from 'express'
+import sqlite3 from 'sqlite3'
+import cors from 'cors'
 
 const app = express()
 const port = 4001
 const db = new sqlite3.Database('memories.db')
 
 app.use(express.json())
+app.use(cors()) // Allow CORS requests
 
 db.serialize(() => {
   db.run(`
@@ -13,13 +15,23 @@ db.serialize(() => {
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       name TEXT,
       description TEXT,
-      timestamp DATE
+      timestamp DATE,
+      imageKey TEXT
     )
   `)
 })
 
 app.get('/memories', (req, res) => {
-  db.all('SELECT * FROM memories', (err, rows) => {
+  const { sort } = req.query
+  let query = 'SELECT * FROM memories'
+
+  if (sort === 'oldest') {
+    query += ' ORDER BY timestamp ASC'
+  } else if (sort === 'newest') {
+    query += ' ORDER BY timestamp DESC'
+  }
+
+  db.all(query, (err, rows) => {
     if (err) {
       res.status(500).json({ error: err.message })
       return
@@ -29,19 +41,20 @@ app.get('/memories', (req, res) => {
 })
 
 app.post('/memories', (req, res) => {
-  const { name, description, timestamp } = req.body
+  const { name, description, timestamp, imageKey } = req.body
 
-  if (!name || !description || !timestamp) {
+  if (!name || !description || !timestamp || !imageKey) {
     res.status(400).json({
-      error: 'Please provide all fields: name, description, timestamp',
+      error:
+        'Please provide all fields: name, description, timestamp, imageKey',
     })
     return
   }
 
   const stmt = db.prepare(
-    'INSERT INTO memories (name, description, timestamp) VALUES (?, ?, ?)'
+    'INSERT INTO memories (name, description, timestamp, imageKey) VALUES (?, ?, ?, ?)'
   )
-  stmt.run(name, description, timestamp, (err) => {
+  stmt.run(name, description, timestamp, imageKey, (err) => {
     if (err) {
       res.status(500).json({ error: err.message })
       return
@@ -67,19 +80,20 @@ app.get('/memories/:id', (req, res) => {
 
 app.put('/memories/:id', (req, res) => {
   const { id } = req.params
-  const { name, description, timestamp } = req.body
+  const { name, description, timestamp, imageKey } = req.body
 
-  if (!name || !description || !timestamp) {
+  if (!name || !description || !timestamp || !imageKey) {
     res.status(400).json({
-      error: 'Please provide all fields: name, description, timestamp',
+      error:
+        'Please provide all fields: name, description, timestamp, imageKey',
     })
     return
   }
 
   const stmt = db.prepare(
-    'UPDATE memories SET name = ?, description = ?, timestamp = ? WHERE id = ?'
+    'UPDATE memories SET name = ?, description = ?, timestamp = ?, imageKey = ? WHERE id = ?'
   )
-  stmt.run(name, description, timestamp, id, (err) => {
+  stmt.run(name, description, timestamp, imageKey, id, (err) => {
     if (err) {
       res.status(500).json({ error: err.message })
       return
